3478cb95147c15e4b8067697fe957d99e67b625d
carlosarriaga83 <cear83@gmail.com>
2025-06-20 00:11:20 -0400

    Update view-event.php

--- view-event.php
+++ view-event.php
@@ -504,7 +504,7 @@
         
         var id = $('input[name="EDIT_ID"]').val();
         debugger;
-        $('input[name="LINK"]').val( 'https://salypimientabanquetes.com/IO3/?e_id=' + id );
+        $('input[name="LINK"]').val( 'https://app.salypimientabanquetes.com/?e_id=%s' + id );
         debugger;
         
     });
